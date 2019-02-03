b921d7c489cdd47786521d192dd337239d5ef758
Ivan Ponomarev <trollvan_123@mail.ru>
2019-02-03 16:20:20 +0300

    Fix twitter

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -7,21 +7,24 @@ module Users
     end
 
     def create
-      Rails.logger.error(session.to_h)
       @user = User.where(user_params).first_or_create
       if @user.new_record?
         @user.password = Devise.friendly_token[0, 20]
         @user.save
         omniauth = session['devise.twitter_data']
-        Rails.logger.error(omniauth)
-        auth = Auth.create(
+        Auth.create(
           provider: omniauth.fetch('provider'),
           uid: omniauth.fetch('uid'),
           user_id: @user.id
         )
-        Rails.logger.error(auth)
         sign_in_and_redirect @user, event: :authentication
       elsif @user.persisted?
+        omniauth = session['devise.twitter_data']
+        Auth.create(
+          provider: omniauth.fetch('provider'),
+          uid: omniauth.fetch('uid'),
+          user_id: @user.id
+        )
         sign_in_and_redirect @user, event: :authentication
       else
         render :new
